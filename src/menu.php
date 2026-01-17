bc458b04d61409fe630e7503c3331076d8b66042
Amanda <amandaveigafurtado@gmail.com>
2026-01-17 19:08:42 -0300

    Mesclei os arquivos menu.php e menu_admin.php

--- src/menu.php
+++ src/menu.php
@@ -1,11 +1,69 @@
-    <nav>
-        <ul class="menuItems">
-            <li><a href='http://localhost/TCC/src/home/home.php' data-item='Home'><i class="fa-solid fa-house fa-sm"></i> Home</a></li>
-            <li><a href='http://localhost/TCC/src/usuario/login.php' data-item='Login | Cadastro'>Login</a></li>
-            <li><a href='http://localhost/TCC/src/receita/busca_ingrediente/busca_ingrediente.php' data-item='Pesquisar Receita por Ingrediente'>Busca por Ingrediente</a></li>
-            <li><a href='http://localhost/TCC/src/receita/listagem_receitas.php' data-item='Livro de Receitas'>Receitas</a></li>
-            <li><a href='http://localhost/TCC/src/receita/cadastrar_receita.php' data-item='Compartilhar Receita'>Postar Receita</a></li>
-            <li><a href='http://localhost/TCC/src/receita/sugestao.php' data-item='Sugestão'>Sugestão</a></li>
-            <li><a href='http://localhost/TCC/src/usuario/dashboard.php' data-item='Meu Perfil'><i class="fa-solid fa-circle-user fa-lg"></i></a></li>
-        </ul>
-    </nav>
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+$isLogged = isset($_SESSION['id_usuario']);
+
+$isAdmin = $isLogged &&
+           isset($_SESSION['statusAdministrador_usuario']) &&
+           $_SESSION['statusAdministrador_usuario'] === 'a';
+?>
+
+<nav>
+    <ul class="menuItems">
+        <li>
+            <a href="/TCC/src/home/home.php">
+                <i class="fa-solid fa-house fa-sm"></i> Home
+            </a>
+        </li>
+
+        <?php if (!$isLogged): ?>
+            <li>
+                <a href="/TCC/src/usuario/login.php">Login</a>
+            </li>
+        <?php endif; ?>
+
+        <li>
+            <a href="/TCC/src/receita/busca_ingrediente/busca_ingrediente.php">
+                Busca por Ingrediente
+            </a>
+        </li>
+
+        <li>
+            <a href="/TCC/src/receita/listagem_receitas.php">Receitas</a>
+        </li>
+
+        <?php if ($isLogged): ?>
+            <li>
+                <a href="/TCC/src/receita/cadastrar_receita.php">
+                    Postar Receita
+                </a>
+            </li>
+
+            <li>
+                <a href="/TCC/src/receita/sugestao.php">Sugestão</a>
+            </li>
+
+            <li>
+                <a href="/TCC/src/usuario/dashboard.php">
+                    <i class="fa-solid fa-circle-user fa-lg"></i>
+                </a>
+            </li>
+        <?php endif; ?>
+
+        <?php if ($isAdmin): ?>
+            <li>
+                <a href="/TCC/src/usuario/listagem_cadastros.php">
+                    Usuários <i class="fa-solid fa-lock-open"></i>
+                </a>
+            </li>
+
+            <li>
+                <a href="/TCC/src/receita/listagem_receitas_admin.php">
+                    Receitas <i class="fa-solid fa-lock-open"></i>
+                </a>
+            </li>
+        <?php endif; ?>
+    </ul>
+</nav>
